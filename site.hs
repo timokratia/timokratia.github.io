6ad03fe2dc445126858d247b759cb34001ed913b
timokratia <timokratia.hu@gmail.com>
2019-03-12 00:10:44 +0000

    Adopt bulma css

--- site.hs
+++ site.hs
@@ -49,7 +49,7 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
+                    constField "title" "Welcome!"            `mappend`
                     defaultContext
 
             getResourceBody
